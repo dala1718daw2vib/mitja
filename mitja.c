a9f1c93200ba3842fbf525575d6e3ba6da74197a
Daniel Laso <dala1718daw2vib@gmail.com>
2018-05-07 12:15:16 +0200

    Tercer commit del programa mitja

--- mitja.c
+++ mitja.c
@@ -1,11 +1,7 @@
-1- Programa mitja (v0.1) 
-2- Per compilar el programa, executa: 
- 	gcc mitja.c -o mitja 
-
 #include <stdio.h> 
  
 int main() { 
- float suma=0; 
+ float suma=0.0; 
  float mitja; 
  float nou_valor; 
  int numero_repeticions=0; 
@@ -13,12 +9,13 @@ int main() {
  
 while(consulta=='s') { 
  printf("DÃ²na la segÃ¼ent dada : "); 
- scanf("%f",&nou_valor); 
- suma=suma+nou_valor; 
- numero_repeticions=numero_repeticions+1; 
- printf("Vols continuar (s/n): "); 
-scanf("%s",&consulta);​ 
- } 
+	 scanf("%f",&nou_valor); 
+	 suma=suma+nou_valor; 
+	 numero_repeticions=numero_repeticions+1; 
+ 	printf("Vols continuar (s/n): "); 
+
+	scanf("%s",&consulta);​ 
+} 
  mitja=suma/numero_repeticions; 
  printf("La mitja resultant Ã©s: %.2f\n",mitja); 
  return 0; 
